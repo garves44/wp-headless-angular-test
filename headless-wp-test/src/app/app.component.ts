fee231add58154c3ad2d3747df501aaf0010bc61
garves <garves44@gmail.com>
2021-02-25 07:35:52 -0700

    headless cms working with angular/wordpress

--- headless-wp-test/src/app/app.component.ts
+++ headless-wp-test/src/app/app.component.ts
@@ -17,8 +17,20 @@ export class AppComponent implements OnInit {
     // need to get the rest API for wordpress still
     this.http.get('http://garvestest.local/wp-json/wp/v2/posts').subscribe(data => {
       // can not itterate over items in object with angular - workaround with projects array
-      for (let key in data) {
+       for (let key in data) {
         if (data.hasOwnProperty(key)) {
+          if(data[key].featured_media !== 0) {
+             this.http.get(`http://garvestest.local/wp-json/wp/v2/media/${data[key].featured_media}`).subscribe(mediaData => {
+              
+              if(mediaData){
+                data[key].media = mediaData
+              } 
+            })
+            
+          }else {
+            data[key].media = {source_url:0}
+          }
+          
           this.projects.push(data[key]);
         }
       }
